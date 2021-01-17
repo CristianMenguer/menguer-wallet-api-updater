c6dd551f604c97a058d75f2a461a29d6f96769f3
Cristian Menguer <cristianmenguer@gmail.com>
2021-01-17 02:11:05 +0000

    run every 3 hours

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,23 @@
+import express, { Response, Request } from 'express'
 import cron from 'node-cron'
 import { updateQuotesService } from './services/UpdateDatabaseAPIService'
 
-cron.schedule('* */3 * * *', () => updateQuotesService())
+const HOSTNAME = '0.0.0.0'
 
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000
+
+cron.schedule('* */3 * * 1-5', () => updateQuotesService())
+
+const app = express()
+
+app.use((request: Request, response: Response) => {
+    console.log('[%s] %s -- %s', new Date(), request.method, request.url)
+    return response.status(404).json({
+        message: 'Welcome to Menguer Wallet - 2020087 API 👍🏼',
+        your_url: request.url
+    })
+})
+
+app.listen(PORT, HOSTNAME, () => {
+    console.log(`> Server started on ${HOSTNAME}:${PORT} 👌`)
+})
