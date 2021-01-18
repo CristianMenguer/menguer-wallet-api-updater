69e40f2d7c9dc2cec8639950afaa76ffd5587344
Cristian Menguer <cristianmenguer@gmail.com>
2021-01-18 21:05:02 +0000

    route to force update

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,15 @@ cron.schedule('* */3 * * 1-5', () => updateQuotesService())
 
 const app = express()
 
+app.get('/checkforupdates', (request: Request, response: Response) => {
+    updateQuotesService()
+    
+    return response.status(404).json({
+        title: 'Welcome to Menguer Wallet - 2020087 API 👍🏼',
+        message: 'This command has forced an update!'
+    })
+})
+
 app.use((request: Request, response: Response) => {
     console.log('[%s] %s -- %s', new Date(), request.method, request.url)
     return response.status(404).json({
